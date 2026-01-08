40ad0d006949eeb28e69b8dac69791d836aa0267
Magnus93 <magnus.ja.gustafsson@gmail.com>
2026-01-08 11:57:49 +0100

    False path, this won't work

--- src/routers/chat.py
+++ src/routers/chat.py
@@ -3,8 +3,20 @@ from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from typing import Literal, Union, List
 from src.core import config
+from src.services import brave_search
 import requests
+import json
 
+
+system_message = {
+    "role": "system",
+    "content": '''
+You have access to a web search tool.
+If you need information from the web, respond ONLY in JSON format like: 
+{"action": "search", "query": "<what to search>"}
+Do NOT output any other text outside this JSON.
+'''
+}
 
 class ContentParts(BaseModel):
     text: str
@@ -12,12 +24,13 @@ class ContentParts(BaseModel):
 
 class MessageRequest(BaseModel):
     content: Union[str, List[ContentParts]]
-    role: Literal["developer", "user", "assistant"]
+    role: Literal["system", "developer", "user", "assistant"]
 
 # https://platform.openai.com/docs/api-reference/chat/create
-class ChatCompletionsRequest(BaseModel):
+class ChatRequest(BaseModel):
     model: str
     messages: List[MessageRequest]
+
 
 
 def stream_ollama(payload):
@@ -26,17 +39,36 @@ def stream_ollama(payload):
         json=payload,
         stream=True
     ) as r:
+        buffer = ""
         for line in r.iter_lines():
+            print("line:\t", line)
             if line:
-                yield line + b"\n"
+                chunk = line.decode("utf-8")
+                buffer += chunk
+
+                try:
+                    data = json.loads(buffer)
+                    if  "action" in data and data["action"] == "search":
+                        query = data["query"]
+                        search_results = brave_search(query)
+                        yield ({"tool": "search", "query": query, "results": search_results}).encode("utf-8") + "b\n"
+                        buffer=""
+                    else:
+                        yield chunk.encode("utf-8") + b"\n"
+                        buffer = ""
+                except json.JSONDecodeError:
+                    yield chunk.encode("utf-8") + b"\n"
 
 
 router = APIRouter(prefix="/api")
 
 @router.post("/chat")
-async def chat(req: ChatCompletionsRequest): 
+async def chat(req: ChatRequest): 
     print("user_message", req.messages[len(req.messages) - 1].content)
     payload = req.model_dump()
+    payload["messages"].append(system_message)
+
+    print("payload:\n", payload)
 
     return StreamingResponse(
         stream_ollama(payload),
---
+++ src/services/brave_search.py
@@ -0,0 +1,34 @@
+
+from typing import List
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+import requests
+from src.core import config
+
+def brave_search(query: str) -> List[dict]:
+    url = f"{config.BRAVE_BASE_URL}/res/v1/web/search"
+    params = { "q": query }
+    headers = {
+        "X-Subscription-Token": config.BRAVE_API_KEY
+    }
+    try:
+        response = requests.get(url, params=params, headers=headers)
+        res_body = response.json()
+        results = res_body.get("web", {}).get("results", [])
+        results2 = [
+            {
+                "title": result["title"], 
+                "description": result["description"]
+            }
+            for result in results    
+        ]
+        print("results2", results2)
+        return results2
+
+
+    except requests.exceptions.HTTPError as e:
+        print("HTTP error:", e)
+    except requests.exceptions.Timeout as e:
+        print("Request timed out:", e)
+    except requests.exceptions.RequestException as e:
+        print("Other error:", e)
