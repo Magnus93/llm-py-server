a98d3446a831e665a11074b2d7ca67d618297625
Magnus93 <magnus.ja.gustafsson@gmail.com>
2026-01-30 23:16:43 +0100

    Testing wikipedia

--- src/wiki.py
+++ src/wiki.py
@@ -9,5 +9,9 @@ results = wikipedia.search("python programming")
 
 wiki = wikipediaapi.Wikipedia(language="en", user_agent="llm-py-server/0.1 (contact: magnus.ja.gustafsson@gmail.com)")
 
-page = wiki.page(results[0])
-print(page.text)
+for result in results:
+    page = wiki.page(result)
+    print(f"\n======= {page.title} =======")
+    print(page.summary)
+    print(page.text)
+    print(page.pageid)
